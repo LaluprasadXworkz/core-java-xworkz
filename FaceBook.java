4bf6f3fec791c8b15ea7b5e5b158e001577791f0
LaluprasadXworkz <LaluprasadXworkz>
2023-08-21 11:44:55 +0530

    methods related example thru facebook logics

---
+++ FaceBook.java
@@ -0,0 +1,13 @@
+class FaceBook{
+	public static void login(String mailID,String passWord){
+		System.out.println("Invoked search:(String mailID,String passWord)");
+		System.out.println("login with mail id");
+		System.out.println("End of (String mailID,String passWord)");
+	}
+	public static void login(long number,String passWord){
+		System.out.println(" ");
+		System.out.println("Invoked search:(long number,String passWord)");
+		System.out.println("login with phone number ");
+		System.out.println("End of  search:(long number,String passWord)");
+}
+}
